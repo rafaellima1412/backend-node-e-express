577d56d1506c85e25464575596d9114cffa669f1
rafaellima1412 <rafaellima1412@gmail.com>
2021-07-09 13:01:22 -0400

    atualizando proejto com mongo

--- index.js
+++ index.js
@@ -1,60 +1,80 @@
 const express = require("express");
-const app = express();
-// aqui informo ao express que no corpo ele vai isar json
-app.use(express.json());
+//configuração do mongodb
+const { MongoClient, ObjectId } = require("mongodb");
+(async () => {
+  const url = "mongodb://localhost:27017";
+  const dbname = "aulafilmes";
 
-app.get("/hello", function (req, res) {
-  res.send("Hello World");
-});
+  console.info("conectando ao banco de dados ....");
+  // retorno do promisse com await ele cumpre a promessa.
+  const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+  console.info("conectado ao Mongo DB");
+  const db = client.db(dbname);
+
+  const app = express();
+  // aqui informo ao express que no corpo ele vai isar json
+  app.use(express.json());
+
+  app.get("/hello", function (req, res) {
+    res.send("Hello World");
+  });
+  // Escolha um filme a lista aqui representa um db
+  const lista = ["senhor dos aneis", "harry porter"];
+  const filmes = db.collections("filmes");
+
+  app.get("/filmes", async (req, res) => {
+    const listaFilmes = await filmes.find().toArray();
+    //um filtro caso os primeiros itens sejam removidos  ou não existam para nao aparecer null
+    res.send(listaFilmes);
+  });
+
+  // [get] read id/index
+  // com arrow function que e a mesma coisa
+  app.get("/filmes/:id", async (req, res) => {
+    const id = req.params.id;
+    //pelo mongoDb
+    const item = await filmes.findOne({ _id: ObjectId(id) });
+    res.send(item);
+  });
+
+  // [post] -create
+  //quando quero criar ou atualizar sempre passo no body usando json
+  app.post("/filmes", async (req, res) => {
+    const item = req.body;
+    await filmes.insertOne(item);
+    res.send(item);
+  });
+
+  // [Put/patch] - Update
+  app.put("/filmes/:id", async (req, res) => {
+    const id = req.params.id;
+    const item = req.body;
+    await filmes.updateOne({ _id: ObjectId(id) }, { $set: item });
+    res.send(item);
+  });
+
+  // [delete] - delete
+  app.delete("/filmes/:id", async (req, res) => {
+    const id = req.params.id;
+    await filmes.deleteOne({ _id: ObjectId(id) });
+    res.send("Item removido com sucesso.");
+  });
+
+  //no geral usando ID eu vou alterar unico elemento.
+  app.listen(3000);
+
+  // return Promise.reject("Oops!").catch((err) => {
+  //   throw new Error(err);
+  // });
+})(); //Async
+
 /* lista de end-point CRUD
-devemos seguir os verbos http
-assim nossa aplicação segue o REST 
-aplicações que seguem o Rest são chamadoas de restfull
-Create -->
-[post] -create 
-[get] - read
-[Put/patch] - Update
-[delete] - delete
-*/
-
-// Escolha um filme a lista aqui representa um db
-const lista = ["senhor dos aneis", "harry porter"];
-app.get("/filmes", function (req, res) {
-  //um filtro caso os primeiros itens sejam removidos  ou não existam para nao aparecer null
-  res.send(lista.filter(Boolean));
-});
-
-// [get] read id/index
-// com arrow function que e a mesma coisa
-app.get("/filmes/:id", (req, res) => {
-  const id = req.params.id;
-  const item = lista[id - 1];
-  res.send(item);
-});
-
-// [post] -create
-//quando quero criar ou atualizar sempre passo no body usando json
-app.post("/filmes", (req, res) => {
-  const item = req.body.nome_filme;
-  lista.push(item);
-  res.send("item criado com sucesso.");
-});
-
-// [Put/patch] - Update
-
-app.put("/filmes/:id", (req, res) => {
-  const id = req.params.id;
-  const item = req.body.nome_filme;
-  lista[id - 1] = item;
-  res.send("item editado com sucesso.");
-});
-
-// [delete] - delete
-app.delete("/filmes/:id", (req, res) => {
-  const id = req.params.id;
-  delete lista[id - 1];
-  res.send("item removido com sucesso.");
-});
-
-//no geral usando ID eu vou alterar unico elemento.
-app.listen(3000);
+  devemos seguir os verbos http
+  assim nossa aplicação segue o REST 
+  aplicações que seguem o Rest são chamadoas de restfull
+  Create -->
+  [post] -create 
+  [get] - read
+  [Put/patch] - Update
+  [delete] - delete
+  */
